20f881f61872f8bea6f52b896a1172d5a58375bb
vinPedro <159270159+vinPedro@users.noreply.github.com>
2024-09-23 10:29:51 -0300

    feat: update Questao2.Programa profile

--- ProjetoGupyEstagio/src/Questao2/Programa.java
+++ ProjetoGupyEstagio/src/Questao2/Programa.java
@@ -1,16 +1,23 @@
 package Questao2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		Scanner inputScanner = new Scanner(System.in);
+			
+		try {
+			System.out.println("Digite um número: ");
+			int entrada = inputScanner.nextInt();
+			System.out.println(QuestãoFibonacci.buscarElemento(entrada));
+		} catch (InputMismatchException e) {
+			System.out.println("Digite um número válido!");
+		}
 		
-		System.out.println("Digite um número: ");
-		int entrada = inputScanner.nextInt();
 		
-		System.out.println(QuestãoFibonacci.buscarElemento(entrada));
+		
 		
 		inputScanner.close();
 
